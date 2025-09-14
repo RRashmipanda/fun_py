70e1649febef251ce49c5916b1d89c922a282e0b
Rashmi Panda <rrashmipanda2400@gmail.com>
2025-09-14 07:22:29 +0530

    yuutube manager in python updated

--- 4_youtube-manager/index.py
+++ 4_youtube-manager/index.py
@@ -1,9 +1,56 @@
+import json
+
+def load_data():
+    try:
+        with open ('youtube.txt','r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []    
+
+# helper method (save videeos in file)
+def save_data_helper(videos):
+    with open('youtube.txt','w') as file:
+        json.dump(videos,file)
+
 
 def list_all_videos(videos):
+    print("\n")
+    print("-" * 20)
+    for index, video in enumerate(videos, start=1):
+        print(f"{index}. {video['name']}, Duration: {video['time']} ")
+    print("\n")
+    print("-" * 50)
 
+def add_video(videos):
+    name = input("Enter video name: ")
+    time = input("Enter video time: ")
+    videos.append({'name': name, 'time': time})
+    save_data_helper(videos)
 
+def update_video(videos):
+    list_all_videos(videos)
+    index = int(input("Enter the video number to update"))
+    if 1 <= index <= len(videos):
+        name = input("Enter the new video name")
+        time = input("Enter the new video time")
+        videos[index-1] = {'name':name, 'time': time}
+        save_data_helper(videos)
+    else:
+        print("Invalid index selected")
 
- while True:
+def delete_video(videos):
+    list_all_videos(videos)
+    index = int(input("Enter the video number to be deleted"))
+    
+    if 1<= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("Invalid video index selected")
+
+def main():
+    videos = load_data()
+    while True:
         print("\n Youtube Manager | choose an option ")
         print("1. List all youtube videos ")
         print("2. Add a youtube video ")
@@ -26,3 +73,7 @@ def list_all_videos(videos):
                 break
             case _:
                 print("Invalid Choice")
+
+if __name__== "__main__":  
+    main()              
+
